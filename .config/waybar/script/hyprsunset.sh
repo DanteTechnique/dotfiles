8f182d6854e1c48d88b4e9c0cabf4437b4a84df3
DanteTechnique <m.everest2015@yandex.ru>
2025-07-19 23:51:19 +0500

    reworked hyprsunset module

--- .config/waybar/script/hyprsunset.sh
+++ .config/waybar/script/hyprsunset.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
 PID_FILE="/tmp/hyprsunset.pid"
+WAYBAR_SIGNAL=1
 
-if [ -f "$PID_FILE" ]; then
-    kill "$(cat "$PID_FILE")" 2>/dev/null
-    rm "$PID_FILE"
-else
-    hyprsunset -t 3500 &
-    echo $! > "$PID_FILE"
-fi
+toggle() {
+    if [ -f "$PID_FILE" ]; then
+        kill "$(cat "$PID_FILE")" 2>/dev/null
+        rm "$PID_FILE"
+        echo '{"text":"","tooltip":"Night Light OFF","class":"disabled"}'
+    else
+        hyprsunset -t 3500 &
+        echo $! > "$PID_FILE"
+        echo '{"text":"","tooltip":"Night Light ON","class":"enabled"}'
+    fi
+    pkill -RTMIN+$WAYBAR_SIGNAL waybar
+}
+
+case "$1" in
+    --update)
+        if [ -f "$PID_FILE" ]; then
+            echo '{"text":"","tooltip":"Night Light ON","class":"enabled"}'
+        else
+            echo '{"text":"","tooltip":"Night Light OFF","class":"disabled"}'
+        fi
+        ;;
+    *)
+        toggle
+        ;;
+esac
