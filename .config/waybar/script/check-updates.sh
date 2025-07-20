e7fc9a2003e9d888cff88e5cccd1be2a802881d1
DanteTechnique <m.everest2015@yandex.ru>
2025-07-20 13:37:44 +0500

    reworked pacman update module

--- .config/waybar/script/check-updates.sh
+++ .config/waybar/script/check-updates.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-#!/bin/bash
-
 check_update () {
-    # Инициализация переменных
     local pacman_updates=0
     local aur_updates=0
     local tooltip="Нет обновлений"
 
-    # Проверка обновлений pacman
     if command -v checkupdates &>/dev/null; then
         pacman_updates=$(checkupdates 2>/dev/null | wc -l)
         pacman_updates=$((pacman_updates + 0))
     fi
 
-    # Проверка обновлений AUR (только yay)
     if command -v yay &>/dev/null; then
         aur_updates=$(yay -Qu 2>/dev/null | wc -l)
         aur_updates=$((aur_updates + 0))
     fi
 
-    # Формирование данных
     local total=$((pacman_updates + aur_updates))
     
     if [ $total -gt 0 ]; then
@@ -31,12 +25,10 @@ check_update () {
     fi
 }
 
-# Функция обновления системы
 update_system() {
     alacritty -e bash -c "~/.config/waybar/script/pacman.sh"
 }
 
-# Обработка аргументов
 case "$1" in
     --update)
         update_system
@@ -46,9 +38,3 @@ case "$1" in
         ;;
 esac
 
-#case $1 in
-#  --update)
-#    alacritty -e bash -c "
-#    ~/.config/waybar/script/pacman.sh
-#  "  
-#esac
