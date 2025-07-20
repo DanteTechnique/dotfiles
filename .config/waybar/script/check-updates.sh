340699548b8d4b1aadb5d830149bfb8277149219
DanteTechnique <m.everest2015@yandex.ru>
2025-07-20 18:44:05 +0500

    reworked hyprsunset module and pacman module

--- .config/waybar/script/check-updates.sh
+++ .config/waybar/script/check-updates.sh
@@ -3,7 +3,8 @@
 check_update () {
     local pacman_updates=0
     local aur_updates=0
-    local tooltip="Нет обновлений"
+    local flatpak_updates=0
+    local tooltip="No available updates"
 
     if command -v checkupdates &>/dev/null; then
         pacman_updates=$(checkupdates 2>/dev/null | wc -l)
@@ -12,13 +13,18 @@ check_update () {
 
     if command -v yay &>/dev/null; then
         aur_updates=$(yay -Qu 2>/dev/null | wc -l)
-        aur_updates=$((aur_updates + 0))
+        aur_updates=$((aur_updates - pacman_updates + 0))
     fi
 
-    local total=$((pacman_updates + aur_updates))
+    if command -v flatpak &>/dev/null; then
+	    flatpak_updates=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
+	    flatpak_updates=$((flatpak_updates + 0))
+    fi
+
+    local total=$((pacman_updates + aur_updates + flatpak_updates))
     
     if [ $total -gt 0 ]; then
-        tooltip="Обновления: Pacman: $pacman_updates | AUR: $aur_updates"
+        tooltip="Packages: Pacman: $pacman_updates | AUR: $aur_updates | Flatpak: $flatpak_updates"
         printf '{"text":"%s","tooltip":"%s"}\n' "$total" "$tooltip"
     else
         echo '{"text":"","tooltip":"No available updates"}'
--- .config/waybar/script/hyprsunset.sh
+++ .config/waybar/script/hyprsunset.sh
@@ -7,11 +7,11 @@ toggle() {
     if [ -f "$PID_FILE" ]; then
         kill "$(cat "$PID_FILE")" 2>/dev/null
         rm "$PID_FILE"
-        echo '{"text":"","tooltip":"Night Light OFF","class":"disabled"}'
+        echo '{"text":"","class":"disabled"}'
     else
         hyprsunset -t 3500 &
         echo $! > "$PID_FILE"
-        echo '{"text":"","tooltip":"Night Light ON","class":"enabled"}'
+        echo '{"text":"","class":"enabled"}'
     fi
     pkill -RTMIN+$WAYBAR_SIGNAL waybar
 }
@@ -19,9 +19,9 @@ toggle() {
 case "$1" in
     --update)
         if [ -f "$PID_FILE" ]; then
-            echo '{"text":"","tooltip":"Night Light ON","class":"enabled"}'
+            echo '{"text":"","class":"enabled"}'
         else
-            echo '{"text":"","tooltip":"Night Light OFF","class":"disabled"}'
+            echo '{"text":"","class":"disabled"}'
         fi
         ;;
     *)
