badad53717b8ae801ecdb5a62e6977a30f7a604b
DanteTechnique <m.everest2015@yandex.ru>
2025-08-11 23:13:45 +0500

    waybar: check-updates.sh

--- .config/waybar/scripts/check-updates.sh
+++ .config/waybar/scripts/check-updates.sh
@@ -1,46 +1,40 @@
 #!/bin/bash
 
-check_update () {
-    local pacman_updates=0
-    local aur_updates=0
-    local flatpak_updates=0
-    local tooltip="No available updates"
+check_update() {
+  local pacman_updates=0 aur_updates=0 flatpak_updates=0
+  local tooltip="No available updates"
 
-    if command -v checkupdates &>/dev/null; then
-        pacman_updates=$(checkupdates 2>/dev/null | wc -l)
-        pacman_updates=$((pacman_updates + 0))
-    fi
+  if command -v checkupdates &>/dev/null; then
+    pacman_updates=$(checkupdates 2>/dev/null | wc -l)
+  fi
 
-    if command -v yay &>/dev/null; then
-        aur_updates=$(yay -Qu 2>/dev/null | wc -l)
-	aur_updates=$((aur_updates + 0))
-    fi
+  if command -v yay &>/dev/null; then
+    aur_updates=$(yay -Qu 2>/dev/null | wc -l)
+  fi
 
-    if command -v flatpak &>/dev/null; then
-	    flatpak_updates=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
-	    flatpak_updates=$((flatpak_updates + 0))
-    fi
+  if command -v flatpak &>/dev/null; then
+    flatpak_updates=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
+  fi
 
-    local total=$((pacman_updates + aur_updates + flatpak_updates))
-    
-    if [ $total -gt 0 ]; then
-        tooltip="Packages: Pacman: $pacman_updates | AUR: $aur_updates | Flatpak: $flatpak_updates"
-        printf '{"text":"%s","tooltip":"%s"}\n' "$total" "$tooltip"
-    else
-        echo '{"text":"","tooltip":"No available updates"}'
-    fi
+  local total=$((pacman_updates + aur_updates + flatpak_updates))
+
+  if [ "$total" -gt 0 ]; then
+    tooltip="Packages: Pacman: $pacman_updates | AUR: $aur_updates | Flatpak: $flatpak_updates"
+    printf '{"text":"%s","tooltip":"%s"}\n' "$total" "$tooltip"
+  else
+    echo '{"text":"","tooltip":"No available updates"}'
+  fi
 }
 
 update_system() {
-    alacritty --class floating -e bash -c "~/.config/waybar/scripts/pacman.sh"
+  alacritty --class floating -e bash -c "~/.config/waybar/scripts/pacman.sh"
 }
 
 case "$1" in
-    --update)
-        update_system
-        ;;
-    *)
-        check_update
-        ;;
+--update)
+  update_system
+  ;;
+*)
+  check_update
+  ;;
 esac
-
--- .config/waybar/scripts/pacman.sh
+++ .config/waybar/scripts/pacman.sh
@@ -9,36 +9,32 @@ handle_error() {
   exit 1
 }
 
-# Pacman
-  print_message " ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ "
-  print_message "█       █       █       █  █▄█  █       █  █  █ █"
-  print_message "█    ▄  █   ▄   █       █       █   ▄   █   █▄█ █"
-  print_message "█   █▄█ █  █▄█  █     ▄▄█       █  █▄█  █       █"
-  print_message "█    ▄▄▄█       █    █  █       █       █  ▄    █"
-  print_message "█   █   █   ▄   █    █▄▄█ ██▄██ █   ▄   █ █ █   █"
-  print_message "█▄▄▄█   █▄▄█ █▄▄█▄▄▄▄▄▄▄█▄█   █▄█▄▄█ █▄▄█▄█  █▄▄█"
-  sudo pacman -Syu || handle_error "Can't update packages"
+print_message " ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ "
+print_message "█       █       █       █  █▄█  █       █  █  █ █"
+print_message "█    ▄  █   ▄   █       █       █   ▄   █   █▄█ █"
+print_message "█   █▄█ █  █▄█  █     ▄▄█       █  █▄█  █       █"
+print_message "█    ▄▄▄█       █    █  █       █       █  ▄    █"
+print_message "█   █   █   ▄   █    █▄▄█ ██▄██ █   ▄   █ █ █   █"
+print_message "█▄▄▄█   █▄▄█ █▄▄█▄▄▄▄▄▄▄█▄█   █▄█▄▄█ █▄▄█▄█  █▄▄█"
+sudo pacman -Syu || handle_error "Can't update packages"
 
-# AUR
-  print_message " ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄ "  
-  print_message "█       █  █ █  █   ▄  █"  
-  print_message "█   ▄   █  █ █  █  █ █ █"  
-  print_message "█  █▄█  █  █▄█  █   █▄▄█▄" 
-  print_message "█       █       █    ▄▄  █"
-  print_message "█   ▄   █       █   █  █ █"
-  print_message "█▄▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█"
-  yay -Syu || handle_error "Can't update AUR packages"
+print_message " ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄ "
+print_message "█       █  █ █  █   ▄  █"
+print_message "█   ▄   █  █ █  █  █ █ █"
+print_message "█  █▄█  █  █▄█  █   █▄▄█▄"
+print_message "█       █       █    ▄▄  █"
+print_message "█   ▄   █       █   █  █ █"
+print_message "█▄▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█"
+yay -Syu || handle_error "Can't update AUR packages"
 
-# Flatpak
-  print_message " ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄▄   ▄ "
-  print_message "█       █   █   █      █       █       █      █   █ █ █"
-  print_message "█    ▄▄▄█   █   █  ▄   █▄     ▄█    ▄  █  ▄   █   █▄█ █"
-  print_message "█   █▄▄▄█   █   █ █▄█  █ █   █ █   █▄█ █ █▄█  █      ▄█"
-  print_message "█    ▄▄▄█   █▄▄▄█      █ █   █ █    ▄▄▄█      █     █▄ "
-  print_message "█   █   █       █  ▄   █ █   █ █   █   █  ▄   █    ▄  █"
-  print_message "█▄▄▄█   █▄▄▄▄▄▄▄█▄█ █▄▄█ █▄▄▄█ █▄▄▄█   █▄█ █▄▄█▄▄▄█ █▄█"
-  flatpak update || handle_error "Can't update Flatpak packages"
-
+print_message " ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄▄   ▄ "
+print_message "█       █   █   █      █       █       █      █   █ █ █"
+print_message "█    ▄▄▄█   █   █  ▄   █▄     ▄█    ▄  █  ▄   █   █▄█ █"
+print_message "█   █▄▄▄█   █   █ █▄█  █ █   █ █   █▄█ █ █▄█  █      ▄█"
+print_message "█    ▄▄▄█   █▄▄▄█      █ █   █ █    ▄▄▄█      █     █▄ "
+print_message "█   █   █       █  ▄   █ █   █ █   █   █  ▄   █    ▄  █"
+print_message "█▄▄▄█   █▄▄▄▄▄▄▄█▄█ █▄▄█ █▄▄▄█ █▄▄▄█   █▄█ █▄▄█▄▄▄█ █▄█"
+flatpak update || handle_error "Can't update Flatpak packages"
 
 print_message "Done..."
 read
