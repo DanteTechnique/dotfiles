da1ef4b09cea89b5d115bff74789c26f8cd1b1e0
DanteTechnique <m.everest2015@yandex.ru>
2025-08-29 13:37:29 +0500

    install.sh
    added:
    - pacman packages
    - AUR helper and AUR packages
    - configuration fish

--- install.sh
+++ install.sh
@@ -1,10 +1,61 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 print_message() {
   txtgreen='\e[0;32m'
-  echo -e "${txtgreen}green print_message"
+  txtreset='\e[0m'
+  echo
+  echo -e "${txtgreen}$1${txtreset}"
 }
 
-print_message
+# installing packages
+
+print_message "installing packages..."
+
+sudo pacman -Syu
+
+sudo pacman -S hyprland \
+  hyprlock \
+  hypridle \
+  hyprsunset \
+  waybar \
+  wl-clipboard \
+  nemo \
+  wofi \
+  alacritty \
+  fish \
+  git \
+  dunst \
+  grim \
+  slurp \
+  network-manager-applet \
+  blueman \
+  tmux \
+  pavucontrol \
+  awesome-terminal-fonts
+
+# installing AUR helper
+
+print_message "installing yay..."
+
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si
+
+# installing AUR packages
+
+print_message "installing AUR packages..."
+
+yay -S pacseek \
+  wlogout \
+  waterfox-bin
+
+# changing shell and installing oh-my-fish
+
+print_message "configuring shell..."
+
+chsh -s /usr/bin/fish $(whoam)
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install boxfish
 
 read
